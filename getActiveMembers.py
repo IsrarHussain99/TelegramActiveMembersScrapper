a9d385ef62e8bb48587328307c6ba6c210a18d04
Israr Hussain <32361952+IsrarHussain99@users.noreply.github.com>
2020-08-09 20:17:05 +0100

    Add files via upload

---
+++ getActiveMembers.py
@@ -0,0 +1,91 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import sys
+import csv
+import traceback
+import time
+from datetime import datetime
+
+api_id = 1007075
+api_hash = '59147df8h87465cbbd2e369f5433c6c8d'
+phone = '+447678952274'
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+ 
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Choose a group to scrape members from:')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
+
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
+
+print('Fetching Members...')
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=True)
+
+print('Saving In file...')
+with open("members_new.csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id','last seen'])
+    for user in all_participants:
+        accept=False
+        try:
+            lastDate=user.status.was_online
+			# change the below 8 and 9 to match the current date 
+			# this allows the program to fetch telegram users depending on date entered.
+            if(lastDate.month == 8 and lastDate.day == 9):
+                accept=True
+        except:
+            continue
+
+        if (accept) :
+            if user.username:
+                username= user.username
+            else:
+                username= ""
+            if user.first_name:
+                first_name= user.first_name
+            else:
+                first_name= ""
+            if user.last_name:
+                last_name= user.last_name
+            else:
+                last_name= ""
+            name= (first_name + ' ' + last_name).strip()
+            writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id,user.status]) 
+
+print('Members scraped successfully.')
